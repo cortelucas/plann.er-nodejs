44d237c0ebf744033e9e2156cb8f2c1a392bb66d
Lucas S. Corte <corte1994.lc@gmail.com>
2024-07-10 14:25:25 -0300

    config: add cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,20 @@
-import fastify from 'fastify'
+import cors from '@fastify/cors';
+import fastify from 'fastify';
+import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod';
+import { confirmTrip, createTrip, healthCheck } from './routes';
 
 const app = fastify()
+
+app.register(cors, {
+  origin: true
+})
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(healthCheck)
+app.register(createTrip)
+app.register(confirmTrip)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('🚀 HTTP server running!')
