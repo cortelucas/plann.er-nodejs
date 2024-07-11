f97e6d65b61038bff4de65d75814c5a52be08029
Lucas S. Corte <corte1994.lc@gmail.com>
2024-07-11 16:47:52 -0300

    feat: implementação da rota que lista os links

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,5 +4,6 @@ export { createActivity } from './create-activity'
 export { createLink } from './create-link'
 export { createTrip } from './create-trip'
 export { getActivities } from './get-activities'
+export { getLinks } from './get-links'
 export { healthCheck } from './health-check'
 
